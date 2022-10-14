5a6e2c0bd19e6f4f71cf21ce4fda1bc540008ceb
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-10-14 21:28:03 +0200

    add first classes

---
+++ Grey Wolf Optimization/src/main/java/Wolf.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Wolf {
+    private ArrayList<Double> positions;
+
+
+    public Wolf(ArrayList<Double> position) {
+        this.positions = position;
+    }
+
+
+}
